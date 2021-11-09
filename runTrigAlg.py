a288bbab8f1fee28df519e039abc8bdfa6e8b557
Daisy Kalra <dkalra@houston.nevis.columbia.edu>
2021-11-09 18:21:25 -0500

    Run Trigger Algorithm

---
+++ runTrigAlg.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import matplotlib.colors as clrs
+import matplotlib.cm as cm
+from matplotlib.collections import PatchCollection
+from matplotlib.patches import Rectangle
+import matplotlib
+import numpy as np
+import csv
+import operator
+import sys
+
+
+sys.path.append(".")
+
+from GenerateTP import GenerateTP
+from datetime import datetime
+start = datetime.now()
+
+inputTP=[]
+#tp = GenerateTP("ChnlMap.txt")
+
+tp = TriggerAlg("NameofInputTPList")
+peaklist = tp.maxADCtp()
+finish = datetime.now()
+
+#print (len(mainlistcol))
+#print(len(stitchedTP))
+#print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!! FIRST HIT !!!!!!!!!!!!!!!!!!!!!!! : " +str(firsthit) )
+#framelist = tp.frameinfo()
+#print(framelist)
+#for x in framelist:
+#    print(x)
+
+print("Start: " + str(start))
+#print("Mid1: " + str(mid1))
+#print("Mid2: " + str(mid2))
+print("Finish: " + str(finish))
+print(finish-start)
+
